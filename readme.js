8ee84ed98d7e55db17d06b9f51e64aab34c09469
hassanbaloch09 <118581188+hassanbaloch09@users.noreply.github.com>
2023-04-01 08:11:48 +0500

    Update readme.js

--- readme.js
+++ readme.js
@@ -1,9 +1,1 @@
-// Responsive Screen and Fonts 
-
-// 1. Installing Dependencies
-// 2. react-native-responsive-screen react-native-responsive-fontsize
-
-
-// 2. Using those dependencies.
-
-// Thanks for Watching !!
+// Thanks for coming
