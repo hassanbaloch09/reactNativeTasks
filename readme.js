7e56d57ec1fe92672798018f5ffced3a965b590f
hassanbaloch09 <hassan.balouch333@gmail.com>
2023-07-03 18:51:06 +0500

    ss

--- readme.js
+++ readme.js
@@ -1,11 +1,1 @@
-// Firebase Authentication
-
-// 1. Creating Firebase App on Browser -- done
-// 2. Adding Firebase in our Project -- done
-// 3. Using Firebase Register and Login Functions
-// Authentication is working!!
-// Lets finalise the features
-
-// Everything is working 
-
-// Thanks for Watching!!
+// Thanks for Visiting
